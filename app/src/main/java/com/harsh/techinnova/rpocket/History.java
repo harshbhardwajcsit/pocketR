2e8bc750c0d42f5a646c3be4eb2e81d2f0d82951
Manish Bisht <manish.bisht490@gmail.com>
2017-03-21 01:14:52 +0530

    history added

--- app/src/main/java/com/harsh/techinnova/rpocket/History.java
+++ app/src/main/java/com/harsh/techinnova/rpocket/History.java
@@ -10,18 +10,18 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
-public class LiveNotification extends Activity {
+public class History extends Activity {
     TextView textView;
     Firebase root;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ntab);
+        setContentView(R.layout.history);
         Firebase.setAndroidContext(this);
         //live notification data base link
         root=new Firebase("https://pocketr-15434.firebaseio.com/");
 
-        textView=(TextView)findViewById(R.id.textview);
+        textView=(TextView)findViewById(R.id.textView);
 
 
     }
@@ -30,16 +30,15 @@ public class LiveNotification extends Activity {
         super.onStart();
 
         //link to the message in the cloud server
-        Firebase nofication=root.child("Message");
+        Firebase nofication=root.child("History");
         nofication.addValueEventListener(new ValueEventListener() {
             @Override
-
-            //on reading data change
             public void onDataChange(DataSnapshot dataSnapshot) {
-
-                textView.setText((CharSequence) dataSnapshot.getValue());
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    LicenceModel post = postSnapshot.getValue(LicenceModel.class);
+                    textView.setText(textView.getText()+post.Request);
+                }
             }
-
             @Override
             public void onCancelled(FirebaseError firebaseError) {
 
